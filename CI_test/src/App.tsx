fd80cb53bba8145405e7fe06520a16e310781d97
FrejaFischer <119223300+FrejaFischer@users.noreply.github.com>
2025-04-07 16:52:28 +0200

    added bug (#2)
    * added bug
    
    * fixed bug?

--- CI_test/src/App.tsx
+++ CI_test/src/App.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
+import ms from "ms";
 
 function App() {
   const [count, setCount] = useState(0);
+  const time = ms("5s");
 
   return (
     <>
@@ -9,6 +11,7 @@ function App() {
         <p>hello again</p>
       </div>
       <h1>Vite + React</h1>
+      <p>{time}</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
       </div>
